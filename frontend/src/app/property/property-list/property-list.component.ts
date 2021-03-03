f8d05c68d6d72bc0c60bff57b8cdb2785b7dd9ac
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-03-03 14:39:28 +1300

    add zooming effect and buttons on images

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -6,6 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      ID: 1,
+      Name: 'Fantastic House',
+      Type: 'House',
+      Price: 150000,
+    },
+    {
+      ID: 2,
+      Name: 'Heritage Property',
+      Type: 'Appartment',
+      Price: 100000,
+    },
+    {
+      ID: 3,
+      Name: 'Coze Home',
+      Type: 'House',
+      Price: 200000,
+    },
+    {
+      ID: 4,
+      Name: 'Pearl Villa',
+      Type: 'House',
+      Price: 500000,
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
