fb538c1ffe04922b3b517b489a17846bd25e2c8c
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-03-04 11:02:45 +1300

    add form validation and custom validator

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,6 +40,7 @@ const appRoutes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [HousingService],
--- frontend/src/app/user/user-signup/user-signup.component.ts
+++ frontend/src/app/user/user-signup/user-signup.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-signup',
   templateUrl: './user-signup.component.html',
-  styleUrls: ['./user-signup.component.css']
+  styleUrls: ['./user-signup.component.css'],
 })
 export class UserSignupComponent implements OnInit {
-
-  constructor() { }
+  signupForm: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup(
+      {
+        userName: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+        confirmPassword: new FormControl(null, [Validators.required]),
+        mobile: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(10),
+        ]),
+      },
+      this.passwordMatchingValidator
+    );
   }
 
+  // Custom validator
+  passwordMatchingValidator(fg: FormGroup): Validators {
+    return fg.get('password').value === fg.get('confirmPassword').value
+      ? null
+      : { notmatched: true };
+  }
+
+  // ------------------------------------
+  // Getter methods for all form controls
+  // ------------------------------------
+  get userName() {
+    return this.signupForm.get('userName') as FormControl;
+  }
+  get email() {
+    return this.signupForm.get('email') as FormControl;
+  }
+  get password() {
+    return this.signupForm.get('password') as FormControl;
+  }
+  get confirmPassword() {
+    return this.signupForm.get('confirmPassword') as FormControl;
+  }
+  get mobile() {
+    return this.signupForm.get('mobile') as FormControl;
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
 }
